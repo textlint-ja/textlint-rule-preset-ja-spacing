ac996b89d4c084501404934900ac4266dd74b739
azu <azuciao@gmail.com>
2016-07-11 00:11:01 +0900

    feat(rule): add ja-no-space-between-half-and-full-width

---
+++ packages/textlint-rule-ja-no-space-between-half-and-full-width/src/textlint-rule-ja-no-space-between-half-and-full-width.js
@@ -0,0 +1,37 @@
+// LICENSE : MIT
+"use strict";
+/*
+ 全角文字と半角文字の間
+ 原則として、全角文字と半角文字の間にスペースを入れません。
+ ただしカタカナ複合語の場合を除きます。
+ */
+import {RuleHelper} from "textlint-rule-helper";
+import {matchCaptureGroupAll} from "match-index";
+function reporter(context) {
+    const {Syntax, RuleError, report, fixer, getSource} = context;
+    const helper = new RuleHelper();
+    return {
+        [Syntax.Str](node){
+            if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
+                return;
+            }
+            let text = getSource(node);
+            // アルファベットと全角の間は半角スペースではない
+            let betweenHanAndZen = matchCaptureGroupAll(text, /[A-Za-z0-9]([ 　])(?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/);
+            let betweenZenAndHan = matchCaptureGroupAll(text, /(?:[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])([ 　])[A-Za-z0-9]/);
+            const reportMatch = (match) => {
+                const {index} = match;
+                report(node, new RuleError("原則として、全角文字と半角文字の間にスペースを入れません。", {
+                    index: match.index,
+                    fix: fixer.replaceTextRange([index, index + 1], "")
+                }));
+            };
+            betweenHanAndZen.forEach(reportMatch);
+            betweenZenAndHan.forEach(reportMatch);
+        }
+    }
+}
+module.exports = {
+    linter: reporter,
+    fixer: reporter
+};
