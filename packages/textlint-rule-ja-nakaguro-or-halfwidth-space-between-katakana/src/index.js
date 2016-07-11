0061d440eb0a3056ef048ab301e4f36981030f02
keik <k4t0.kei@gmail.com>
2016-07-12 01:14:41 +0900

    feat(rule): add ja-nakaguro-or-halfwidth-space-between-katakana

---
+++ packages/textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana/src/index.js
@@ -0,0 +1,31 @@
+// LICENSE : MIT
+"use strict";
+/*
+ カタカナ語間のスペースの有無
+ 中黒または半角スペースを用いてカタカナ語を区切ります。
+ */
+import {RuleHelper} from "textlint-rule-helper";
+import {matchCaptureGroupAll} from "match-index";
+export default function (context) {
+    const {Syntax, RuleError, report, getSource} = context;
+    const helper = new RuleHelper();
+    return {
+        [Syntax.Str](node){
+            if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
+                return;
+            }
+            const text = getSource(node);
+            // カタカナ(カタカナ以外)カタカナ のパターンを取り出す
+            matchCaptureGroupAll(text, /[ァ-ヶー]([^[ァ-ヶー])[ァ-ヶー]/).forEach(match => {
+                // カタカナの間を全角スペースでは区切らない
+                const {text} = match;
+                if (text === "　") {
+                    report(node, new RuleError("カタカナ語間は中黒（・）または半角スペースを用いてカタカナ語を区切ります", {
+                        index: match.index
+                    }));
+                }
+            });
+
+        }
+    };
+}
---
+++ packages/textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana/test/index-test.js
@@ -0,0 +1,25 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/index";
+var tester = new TextLintTester();
+tester.run("カタカナ語間のスペースの有無", rule, {
+    valid: [
+        "カタカナ・カタカナ",
+        "カタカナ カタカナ",
+        "カタカナ、カタカナ",// 例外としてしょうがない気がする
+        "あいう　えお",
+        "インターフェース ブラウザ"
+    ],
+    invalid: [
+        {
+            text: "カタカナ　カタカナ",
+            errors: [
+                {
+                    message: "カタカナ語間は中黒（・）または半角スペースを用いてカタカナ語を区切ります",
+                    column: 5
+                }
+            ]
+        }
+    ]
+});
