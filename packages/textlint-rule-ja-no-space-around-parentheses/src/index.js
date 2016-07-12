19cd32d7f92696eb4ce1632d43c534cfce48bc98
KATO Kei <k4t0.kei@gmail.com>
2016-07-12 23:11:12 +0900

    feat(rule): add no-space-around-parentheses (#4)

---
+++ packages/textlint-rule-ja-no-space-around-parentheses/src/index.js
@@ -0,0 +1,55 @@
+// LICENSE : MIT
+"use strict";
+/*
+ かっこ類と隣接する文字の間のスペースの有無
+ かっこの外側、内側ともにスペースを入れません。
+ */
+import {RuleHelper} from "textlint-rule-helper";
+import {matchCaptureGroupAll} from "match-index";
+
+const brackets = [
+    "\\[", "\\]", "（", "）", "［", "］", "「", "」", "『", "』"
+];
+
+const leftBrackets = brackets.map(bracket => {
+    return new RegExp("\([ 　]\)" + bracket, "g");
+});
+const rightBrackets = brackets.map(bracket => {
+    return new RegExp(bracket + "\([ 　])", "g");
+});
+function reporter(context) {
+    const {Syntax, RuleError, report, fixer, getSource} = context;
+    const helper = new RuleHelper();
+    return {
+        [Syntax.Str](node){
+            if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
+                return;
+            }
+            const text = getSource(node);
+            // 左にスペース
+            leftBrackets.forEach(pattern => {
+                matchCaptureGroupAll(text, pattern).forEach(match => {
+                    const {index} = match;
+                    report(node, new RuleError("かっこの外側、内側ともにスペースを入れません。", {
+                        index: index,
+                        fix: fixer.replaceTextRange([index, index + 1], "")
+                    }));
+                });
+            });
+            // 右にスペース
+            rightBrackets.forEach(pattern => {
+                matchCaptureGroupAll(text, pattern).forEach(match => {
+                    const {index, text} = match;
+                    report(node, new RuleError("かっこの外側、内側ともにスペースを入れません。", {
+                        index: index,
+                        fix: fixer.replaceTextRange([index, index + 1], "")
+                    }));
+                });
+            });
+        }
+    };
+}
+export default {
+    linter: reporter,
+    fixer: reporter
+};
