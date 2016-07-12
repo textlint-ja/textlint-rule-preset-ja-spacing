838faebf25f78a876411c45277ab4255b8505b0d
keik <k4t0.kei@gmail.com>
2016-07-12 22:00:56 +0900

    feat(rule): add no-space-around-parentheses

---
+++ packages/textlint-rule-ja-no-space-around-parentheses/test/index-test.js
@@ -0,0 +1,57 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/index";
+var tester = new TextLintTester();
+tester.run("かっこ類と隣接する文字の間のスペースの有無", rule, {
+    valid: [
+        "「良い」",
+        "テスト［文章］です",
+        `
+実装をみてもらうと分かりますが、JavaScriptの\`prototype\`の仕組みをそのまま利用しています。
+そのため、特別な実装は必要なく
+「拡張する時は\`calculator.prototype\`の代わりに\`calculator.fn\`を拡張してください」
+というルールがあるだけとも言えます。
+`
+    ],
+    invalid: [
+        {
+            text: "「 ダメ」",
+            output: "「ダメ」",
+            errors: [
+                {message: "かっこの外側、内側ともにスペースを入れません。"}
+            ]
+        },
+        {
+            text: "これは 「ダメ」です",
+            output: "これは「ダメ」です",
+            errors: [
+                {
+                    message: "かっこの外側、内側ともにスペースを入れません。",
+                    line: 1,
+                    column: 4
+                }
+            ]
+        },{
+            text: `TEST
+
+- TODO
+
+これは 「ダメ」です
+`,
+            output: `TEST
+
+- TODO
+
+これは「ダメ」です
+`,
+            errors: [
+                {
+                    message: "かっこの外側、内側ともにスペースを入れません。",
+                    line: 5,
+                    column: 4
+                }
+            ]
+        }
+    ]
+});
