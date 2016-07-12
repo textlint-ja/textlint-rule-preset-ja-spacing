456172ae8e7a285c126f442c7ce3b4b5c70f6519
KATO Kei <k4t0.kei@gmail.com>
2016-07-12 23:05:22 +0900

    feat(rule): add space-after-question (#5)

---
+++ packages/textlint-rule-ja-space-after-question/test/index-test.js
@@ -0,0 +1,26 @@
+// LICENSE : MIT
+"use strict";
+import TextLintTester from "textlint-tester";
+import rule from "../src/index";
+var tester = new TextLintTester();
+tester.run("疑問符(?)", rule, {
+    valid: [
+        "【原文】Does the reader understand the document?",
+        "【訳文】読者は文書の内容を理解しているでしょうか。",
+        "オプションを変更しますか？",
+        "A 社の成功の秘密とは？　この本ではそれをご紹介します。",
+        "どう操作したらよいのか？というユーザーの疑問に答えます。"
+    ],
+    invalid: [
+        {
+            text: "驚きの速さ！？ これが新製品のキャッチコピーでした？　これは問題なし",
+            output: "驚きの速さ！？　これが新製品のキャッチコピーでした？　これは問題なし",
+            errors: [
+                {
+                    message: "文末に感嘆符を使用し、後に別の文が続く場合は、直後に全角スペースを挿入します。",
+                    column: 8
+                }
+            ]
+        }
+    ]
+});
