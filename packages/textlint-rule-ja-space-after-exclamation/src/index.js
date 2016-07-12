01d0a873eb782538f275b699ef935ccbc9d9b0c6
keik <k4t0.kei@gmail.com>
2016-07-12 21:35:42 +0900

    feat(rule): add space-after-exclamation

---
+++ packages/textlint-rule-ja-space-after-exclamation/src/index.js
@@ -0,0 +1,35 @@
+// LICENSE : MIT
+"use strict";
+/*
+ 感嘆符(!)
+ 文末に感嘆符を使用し、後に別の文が続く場合は、直後に全角スペースを挿入します。
+ 文中に感嘆符を使用する場合はスペースを挿入しません。
+ */
+import {RuleHelper} from "textlint-rule-helper";
+import {matchCaptureGroupAll} from "match-index";
+function reporter(context) {
+    const {Syntax, RuleError, report, fixer, getSource} = context;
+    const helper = new RuleHelper();
+    return {
+        [Syntax.Str](node){
+            if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
+                return;
+            }
+            let text = getSource(node);
+            // ！の後ろは全角スペースが推奨
+            // 半角スペースである場合
+            const matchAfter = /！( )[^\n]/;
+            matchCaptureGroupAll(text, matchAfter).forEach(match => {
+                const {index} = match;
+                return report(node, new RuleError("文末に感嘆符を使用し、後に別の文が続く場合は、直後に全角スペースを挿入します。", {
+                    index: index,
+                    fix: fixer.replaceTextRange([index, index + 1], "　")
+                }));
+            });
+        }
+    };
+}
+export default {
+    linter: reporter,
+    fixer: reporter
+};
