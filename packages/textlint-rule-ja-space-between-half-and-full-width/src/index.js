3221b07c5dac638556093c33e60e3b3883f7ac47
Sho Ito <i.sho.0628@gmail.com>
2023-01-22 18:20:50 +0900

    feat(space-between-half-and-full-width): add array format for space options
    - allow to pass space options as an array
      - e.g. ['alphabets', 'numbers', 'punctuation']
    
    Refs: https://github.com/textlint-ja/textlint-rule-preset-ja-spacing/pull/45#discussion_r1071956677

--- packages/textlint-rule-ja-space-between-half-and-full-width/src/index.js
+++ packages/textlint-rule-ja-space-between-half-and-full-width/src/index.js
@@ -8,35 +8,48 @@ import {RuleHelper} from "textlint-rule-helper";
 import {matchCaptureGroupAll} from "match-index";
 const PunctuationRegExp = /[。、]/;
 const ZenRegExpStr = '[、。]|[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ]'
+const spaceOptions = {
+  always: ['alphabets', 'numbers', 'punctuation'],
+  never: [],
+};
 const defaultOptions = {
     // スペースを入れるかどうか
-    // "never" or "always"
-    space: "never",
-    // 半角数字を無視するかどうか
-    // only works w/ space: "always"
-    ignoreNumbers: false,
+    // ("never"|"always"|Array)
+    space: spaceOptions.never,
     // [。、,.]を例外とするかどうか
     exceptPunctuation: true,
     // プレーンテキスト以外を対象とするか See https://github.com/textlint/textlint-rule-helper#rulehelperisplainstrnodenode-boolean
     lintStyledNode: false,
 };
 function reporter(context, options = {}) {
+   /**
+    * 入力された `space` オプションを内部処理用に成形する
+    * @param {string|Array|undefined} opt `space` オプションのインプット
+    * @returns {Array}
+    */
+    const parseSpaceOption = (opt) => {
+      let option = defaultOptions.space;
+      if (typeof opt === 'string') {
+         assert(opt === "always" || opt === "never", `"space" options should be "always", "never" or an array.`);
+        if (opt === 'always') option = spaceOptions.always
+      } else if (Array.isArray(opt)) {
+         assert(opt.every((v) => spaceOptions.always.includes(v)), `Only "alphabets", "numbers", "punctuation" should be included in the array.`);
+         option = opt;
+      }
+
+      return option;
+    }
+
     const {Syntax, RuleError, report, fixer, getSource} = context;
     const helper = new RuleHelper();
-    const spaceOption = options.space || defaultOptions.space;
-    const ignoreNumbers = options.ignoreNumbers !== undefined
-        ? options.ignoreNumbers
-        : defaultOptions.ignoreNumbers;
-    const exceptPunctuation = options.exceptPunctuation !== undefined
+    const spaceOption = parseSpaceOption(options.space);
+    let exceptPunctuation = spaceOption.includes('punctuation') ? true : undefined;
+    exceptPunctuation = options.exceptPunctuation !== undefined
         ? options.exceptPunctuation
         : defaultOptions.exceptPunctuation;
     const lintStyledNode = options.lintStyledNode !== undefined
         ? options.lintStyledNode
         : defaultOptions.lintStyledNode;
-    assert(spaceOption === "always" || spaceOption === "never", `"space" options should be "always" or "never".`);
-    if (spaceOption !== "always") {
-      assert(ignoreNumbers === false, `"ignoreNumbers" option can only work with "always" for the space option.`);
-    }
     /**
      * `text`を対象に例外オプションを取り除くfilter関数を返す
      * @param {string} text テスト対象のテキスト全体
@@ -76,19 +89,35 @@ function reporter(context, options = {}) {
     };
 
     // Always: アルファベットと全角の間はスペースを入れる
-    const needSpaceBetween = (node, text, ignoreNumbers) => {
-        const betweenHanAndZenRegExpStr = ignoreNumbers
-            ? `([A-Za-z])(?:${ZenRegExpStr})`
-            : `([A-Za-z0-9])(?:${ZenRegExpStr})`
-        const betweenZenAndHanRegExpStr = ignoreNumbers
-            ? `(${ZenRegExpStr})[A-Za-z]`
-            : `(${ZenRegExpStr})[A-Za-z0-9]`
-        const errorMsg = ignoreNumbers
-            ? "原則として、全角文字と数字以外の半角文字の間にスペースを入れます。"
-            : "原則として、全角文字と半角文字の間にスペースを入れます。"
+    const needSpaceBetween = (node, text, options) => {
+       /**
+        * オプションを元に正規表現オプジェクトを生成する
+        * @param {Array} opt `space` オプション
+        * @param {boolean} btwHanAndZen=true 半角全角の間か全角半角の間か
+        * @returns {Object}
+        */
+        const generateRegExp = (opt, btwHanAndZen = true) => {
+          const alphabets = 'A-Za-z';
+          const numbers = opt.includes('numbers') ? '0-9' : '';
 
-        const betweenHanAndZen = matchCaptureGroupAll(text, new RegExp(betweenHanAndZenRegExpStr));
-        const betweenZenAndHan = matchCaptureGroupAll(text, new RegExp(betweenZenAndHanRegExpStr));
+          let expStr;
+          if (btwHanAndZen) {
+            expStr = `([${alphabets}${numbers}])(?:${ZenRegExpStr})`;
+          } else {
+            expStr = `(${ZenRegExpStr})[${alphabets}${numbers}]`;
+          }
+
+          return new RegExp(expStr);
+        };
+
+        const betweenHanAndZenRegExp = generateRegExp(options);
+        const betweenZenAndHanRegExp = generateRegExp(options, false);
+        const errorMsg = options.includes('numbers')
+            ? "原則として、全角文字と半角文字の間にスペースを入れます。"
+            : "原則として、全角文字と数字以外の半角文字の間にスペースを入れます。"
+
+        const betweenHanAndZen = matchCaptureGroupAll(text, betweenHanAndZenRegExp);
+        const betweenZenAndHan = matchCaptureGroupAll(text, betweenZenAndHanRegExp);
         const reportMatch = (match) => {
             const {index} = match;
             report(node, new RuleError(errorMsg, {
@@ -106,10 +135,10 @@ function reporter(context, options = {}) {
             }
             const text = getSource(node);
 
-            if (spaceOption === "always") {
-                needSpaceBetween(node, text, ignoreNumbers)
-            } else if (spaceOption === "never") {
+          if (spaceOption.filter(opt => opt !== 'punctuation').length === 0) {
                 noSpaceBetween(node, text);
+            } else {
+                needSpaceBetween(node, text, spaceOption)
             }
 
         }
