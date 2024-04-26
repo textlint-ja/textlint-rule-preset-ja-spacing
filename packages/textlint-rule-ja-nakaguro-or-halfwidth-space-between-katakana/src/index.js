f272eb17d19f92ae4fbfd060fce8229e6ce57c02
piyo <chick.p.1120@gmail.com>
2024-04-26 18:23:51 +0900

    fix(ja-nakaguro-or-halfwidth-space-between-katakana): replace match-index with String.prototype.matchAll

--- packages/textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana/src/index.js
+++ packages/textlint-rule-ja-nakaguro-or-halfwidth-space-between-katakana/src/index.js
@@ -5,7 +5,7 @@
  中黒または半角スペースを用いてカタカナ語を区切ります。
  */
 import { RuleHelper } from "textlint-rule-helper";
-import { matchCaptureGroupAll } from "match-index";
+
 module.exports = function (context) {
     const { Syntax, RuleError, report, getSource } = context;
     const helper = new RuleHelper();
@@ -16,18 +16,18 @@ module.exports = function (context) {
             }
             const text = getSource(node);
             // カタカナ(カタカナ以外)カタカナ のパターンを取り出す
-            matchCaptureGroupAll(text, /[ァ-ヶー]([^[ァ-ヶー])[ァ-ヶー]/).forEach((match) => {
+            for (const match of text.matchAll(/[ァ-ヶー]([^[ァ-ヶー])[ァ-ヶー]/g)) {
                 // カタカナの間を全角スペースでは区切らない
-                const { text } = match;
+                const text = match[1];
                 if (text === "　") {
                     report(
                         node,
                         new RuleError("カタカナ語間は中黒（・）または半角スペースを用いてカタカナ語を区切ります", {
-                            index: match.index
+                            index: match.index + 1
                         })
                     );
                 }
-            });
+            }
         }
     };
 };
