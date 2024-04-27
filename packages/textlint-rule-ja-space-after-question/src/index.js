bd59660d63f864eea544b95018de0e1164bdff23
piyo <chick.p.1120@gmail.com>
2024-04-27 10:01:11 +0900

    refactor(ja-space-after-question): replace match-index with String.prototype.matchAll

--- packages/textlint-rule-ja-space-after-question/src/index.js
+++ packages/textlint-rule-ja-space-after-question/src/index.js
@@ -6,7 +6,6 @@
  文中に疑問符を使用する場合はスペースを挿入しません。
  */
 import { RuleHelper } from "textlint-rule-helper";
-import { matchCaptureGroupAll } from "match-index";
 function reporter(context) {
     const { Syntax, RuleError, report, fixer, getSource } = context;
     const helper = new RuleHelper();
@@ -18,17 +17,17 @@ function reporter(context) {
             let text = getSource(node);
             // ？の後ろは全角スペースが推奨
             // 半角スペースである場合はエラーとする
-            const matchAfter = /？( )[^\n]/;
-            matchCaptureGroupAll(text, matchAfter).forEach((match) => {
-                const { index } = match;
-                return report(
+            const matchAfter = /？( )[^\n]/g;
+            for (const match of text.matchAll(matchAfter)) {
+                const indexOneBased = match.index + 1;
+                report(
                     node,
                     new RuleError("文末に疑問符を使用し、後に別の文が続く場合は、直後に全角スペースを挿入します。", {
-                        index: index,
-                        fix: fixer.replaceTextRange([index, index + 1], "　")
+                        index: indexOneBased,
+                        fix: fixer.replaceTextRange([indexOneBased, indexOneBased + 1], "　")
                     })
                 );
-            });
+            }
         }
     };
 }
